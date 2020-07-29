a245653312ddd0a3fa399faaa53d099a096a1495
Anna <nuleeannajeon@gmail.com>
2020-07-28 22:16:39 -0400

    added Google Books API

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import API from './utils/API';
 
 function App() {
+  async function getGoogleBooksData() {
+    try {
+      const response = await API.getGoogleBook('Flower');
+      console.log( response )
+      //Get required info
+      const results = response.data.items.forEach( result=> (
+        console.log( `Title: ${result.volumeInfo.title}, 
+          Author(s): ${result.volumeInfo.authors},
+          description: ${result.volumeInfo.description}, 
+          Image: ${result.volumeInfo.imageLinks.smallThumbnail},
+          Link: ${result.volumeInfo.previewLink}` )
+      ))
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect( ()=>{
+    getGoogleBooksData()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ client/src/utils/API.js
@@ -0,0 +1,25 @@
+require( 'dotenv' ).config();
+const axios = require('axios');
+
+// * `/api/books` (get) - Should return all saved books as JSON.
+
+// * `/api/books` (post) - Will be used to save a new book to the database.
+
+// * `/api/books/:id` (delete) - Will be used to delete a book from the database by Mongo `_id`.
+
+export default {
+    getGoogleBook: function(bookName){
+        return axios.get(
+            `https://www.googleapis.com/books/v1/volumes?q=${bookName}&printType=books&maxResults=10&key=`, process.env.REACT_APP_GOOGLE_API_KEY
+        );
+    },
+    getBook: function(){
+        return axios.get('/api/books');
+    },
+    saveBook: function(book){
+        return axios.post('/api/books', book);
+    },
+    deleteBook: function(bookId){
+        return axios.delete('/api/books/', bookId);
+    }
+}

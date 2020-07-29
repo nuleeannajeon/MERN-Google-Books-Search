9396de4f49dbe037900fa94f2c2f311ec0a54f2a
Anna <nuleeannajeon@gmail.com>
2020-07-28 20:52:27 -0400

    basic outline/ created react-app

---
+++ models/books.js
@@ -0,0 +1,15 @@
+// 2. Add code to connect to a MongoDB database named `googlebooks` using the mongoose npm package.
+
+// 3. Using mongoose, then create a Book schema.
+
+// 4. At a minimum, books should have each of the following fields:
+
+// * `title` - Title of the book from the Google Books API
+
+// * `authors` - The books's author(s) as returned from the Google Books API
+
+// * `description` - The book's description as returned from the Google Books API
+
+// * `image` - The Book's thumbnail image as returned from the Google Books API
+
+// * `link` - The Book's information link as returned from the Google Books API
---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// Serve up static assets (usually on heroku)
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
+
+//Will load your single HTML page in `client/build/index.html`.
+// app.get('*', (req, res) => {
+//     res.sendFile('client/build/index.html', { root: __dirname });
+// });
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});

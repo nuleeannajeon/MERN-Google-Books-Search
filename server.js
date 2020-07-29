7b9124b4e061a6f03e9edbeb5e8a89fd2dbe008d
Anna <nuleeannajeon@gmail.com>
2020-07-28 21:05:39 -0400

    basic server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((err, req, res, next) => {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 // Start the API server
 app.listen(PORT, function() {
